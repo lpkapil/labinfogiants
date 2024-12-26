c606f6c4a01eaf1442c983dec9e5746f65a9c89d
Kapil Yadav <lpkapil@gmail.com>
2024-12-26 13:01:32 +0530

    Update test_env.py

--- scripts/test_env.py
+++ scripts/test_env.py
@@ -1,5 +1,10 @@
 import os
 import django
+import sys
+
+# Add the parent directory of 'myproject' to the Python path
+# This allows us to import 'myproject' and access the settings
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Set the DJANGO_SETTINGS_MODULE environment variable
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
